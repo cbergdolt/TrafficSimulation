a575b68295927409410f139d64361a58568da867
Shelby Lem <slem@nd.edu>
2017-11-05 15:19:17 -0500

    Added method shells to vehicle

--- Simulation/src/application/Vehicle.java
+++ Simulation/src/application/Vehicle.java
@@ -1,5 +1,70 @@
 package application;
 
-public class Vehicle {
+import java.awt.Point;
+import java.util.Observer;
 
+public class Vehicle {
+	int maxVelocity;
+	int breakDistance;
+	int stopDistance;
+	int length;
+	char direction;
+	Point location;
+	Point route[];
+	
+	void removeObserver(Observer o) {
+		
+	}
+	
+	void updateObservers() {
+		
+	}
+
+	void addObserver(Observer o) {
+		
+	}
+	
+	void setMaxVelocity(int v) {
+		this.maxVelocity = v;
+	}
+	
+	void setBreakDist(int d) {
+		this.breakDistance = d;
+	}
+	
+	void setStopDist(int d) {
+		this.stopDistance = d;
+	}
+	
+	void setLength(int l) {
+		this.length = l;
+	}
+	
+	void updatePos(int grid[][]) {
+		
+	}
+	
+	void setRoute() {
+		
+	}
+	
+	void stop() {
+		
+	}
+	
+	void decelerate() {
+		
+	}
+	
+	void changeState() {
+		
+	}
+	
+	void turn(String direcion) {
+		
+	}
+	
+	void start() {
+		
+	}
 }
