8cc462a04e36a9569931887835a0c7898972ecce
Carolyn <Carolyn@SamwiseGamgee.dhcp.nd.edu>
2017-11-05 21:51:48 -0500

    Basic UserInterface and Simulation communication

--- Simulation/src/application/Simulation.java
+++ Simulation/src/application/Simulation.java
@@ -1,6 +1,8 @@
 package application;
 
-public class Simulation {
+import java.util.Observable;
+
+public class Simulation extends Observable{
 	int runTime;
 	int delay;
 	int stepLength;
@@ -9,7 +11,30 @@ public class Simulation {
 	Vehicle[] vehicles;
 	RouteGenerator rg;
 	
-	void main() {
+	Simulation() {
+		runTime = 25;
+		stepLength = 500; //500 milliseconds (half a second)
+		//other pesky initialization things
+	}
+	
+	void run() {
+		int time = 0;
+		while(time <= runTime) { //I honestly have no idea. But I think there should be a loop controlling the marjority of the simulation 
+			//sleep
+			try {
+				Thread.sleep(stepLength);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("hey! let me sleep!"); //I'm not sure what we actually want to do with an interrupted sleep, but here's the backbone anyway
+				e.printStackTrace();
+			}
+			
+			//for each vehicle, update the vehicle, based on the current time
+			//update other things, too, like the stoplights
+			// generate new vehicles (and so also routes)
+			time++;
+			notifyObservers();
+		}
 		
 	}
 	
--- Simulation/src/application/UserInterface.java
+++ Simulation/src/application/UserInterface.java
@@ -1,25 +1,84 @@
 package application;
 
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class UserInterface {
+public class UserInterface extends Application implements Observer{
 	ImageView views[];
 	Image images[];
-	int dimensions;
-	int scale;
+	int dimensions = 50;
+	int scale = 25;
+	
+	ObservableList<Node> obsList;
+	
 	
 	void display() {
-		
+		//If the entire map is going to be images, I don't think we need this function
+		//  we can just change the location of the images when necessary, and javafx magic takes care of displaying them?
+		//On the other hand, if we need actual map data to do this display, that might be a problem;
+		//	the only communication line between the two classes is observer/observable relationship right now.
 	}
 	
 	void startSimulation(){
-		
+		//I also don't think this function is necessary
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		launch(args);
 	}
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane root = new BorderPane();
+		obsList = root.getChildren();
+		
+		initialize();
+		
+		Scene scene = new Scene(root,400,400);
+		//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Traffic Simulation");
+		primaryStage.show();	//this should be the blank map, ready to go
+		
+		
+		//somehow before this point, and probably even before we display the initial map, 
+		//	we need to get user input on the map/sim specs. 
+		//	and then perhaps these specs should be passed to the Simulation constructor
+		Simulation sim = new Simulation();
+		sim.run();	//run the simulation!!!
+		
+		//once the sim finishes, close the stage
+		primaryStage.close();
+	}
+
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		//import/create images and image views, add to ObsList
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		//update the images/image views based on the status of everything in the Simulation (the Observable o)
+		
+		
+		//then, after everything is updated properly:
+		display();
+	}
+
+
 
 }
