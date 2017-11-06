f7f99728d56ccf38925eaf52c64c3446b45f1621
Carolyn <Carolyn@SamwiseGamgee.dhcp.nd.edu>
2017-11-05 21:55:34 -0500

    Map with comments

--- Simulation/src/application/Map.java
+++ Simulation/src/application/Map.java
@@ -6,10 +6,18 @@ public class Map {
 	RoadSegment[] roads;
 	Landmark[] landmarks;	
 	
-	public static void main(String[] args) {
+	Map() {
+		//I guess this is where the grid(s) are hard-coded
+		//And are the landmarks generated, or passed in?
+		// once we have the list of landmarks, we can iterate through that and place them on the grid(s)
+		//I'm also not sure if we need the road segments here?
+	}
+	
+	//I don't think we really want a main function in the Map class. The map doesn't really do anything...
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+	}*/
 
 	public void updateMap() {
 		
