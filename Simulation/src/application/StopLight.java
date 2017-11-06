ea0d1dbc504ca052f90eee5f46ef5cf3a8a3bb84
Carolyn <Carolyn@SamwiseGamgee.dhcp.nd.edu>
2017-11-05 21:54:34 -0500

    stoplight states and update function

--- Simulation/src/application/StopLight.java
+++ Simulation/src/application/StopLight.java
@@ -6,14 +6,60 @@ public class StopLight {
 	LightState state;
 	int id;
 	Point location;
-	int timePassed;
-	int greenNS;
-	int yellowNS;
-	int greenEW;
-	int yellowEW;
+	int timePassed; //since last light change
+	int greenNS; // time spent in state GNS_REW
+	int yellowNS; // time spent in state YNS_REW
+	int greenEW; // time spent in state RNS_GEW
+	int yellowEW; // time spent in state RNS_YEW
 	
-	void updateState() {
+	StopLight(LightState initialState, Point loc, int gns, int yns, int gew, int yew) {
+		state = initialState;
+		//id = ???
+		location = loc;
+		timePassed = 0;
+		greenNS = gns;
+		yellowNS = yns;
+		greenEW = gew;
+		yellowEW = yew;
+	}
+	
+	LightState getState() {
+		return state;
+	}
+	
+	void update() {
 		//updates the state of the light based on the time passed since last state change
-		//and the static values of greenNS/EW and yellowNS/EW
+		//and the static values of greenNS/EW / yellowNS/EW
+		timePassed++; //increment time
+		
+		switch(state) {
+		case GNS_REW:
+			if (timePassed > greenNS) {
+				state = LightState.YNS_REW; // turn NS light yellow
+				timePassed = 0;
+			}
+			break;
+		case YNS_REW:
+			if (timePassed > yellowNS) {
+				state = LightState.RNS_GEW; // turn NS light red and EW light green
+				timePassed = 0;
+			}
+			break;
+		case RNS_GEW:
+			if (timePassed > greenEW) {
+				state = LightState.RNS_YEW; // turn EW light yellow
+				timePassed = 0;
+			}
+			break;
+		case RNS_YEW:
+			if (timePassed > yellowEW) {
+				state = LightState.GNS_REW; // turn NS light green and EW light red
+				timePassed = 0;
+			}
+			break;
+		default:
+			//if the state is something other than these four options, we have a serious issue...
+			System.out.println("something has gone horribly wrong"); 
+		}
 	}
 }
