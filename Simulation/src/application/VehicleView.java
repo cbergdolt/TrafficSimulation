1cf3c45141de8aa5b17a415ab5e94ac92892a0a4
phale01 <phale01@saintmarys.edu>
2017-11-08 18:14:11 -0500

    more things changed

--- Simulation/src/application/VehicleView.java
+++ Simulation/src/application/VehicleView.java
@@ -7,12 +7,14 @@ public class VehicleView {
 	Vehicle vehicle;
 	ImageView imageView;
 	int moveCount;
+	boolean inSimulation;
 	
 	VehicleView(Vehicle veh) {
 		//int scale = 15; //hard coded to match the scale in UserInterface
 		vehicle = veh;
-		imageView = null;
-	
+		imageView = null;	
+		inSimulation = true;
+		moveCount = 0;
 		//Image im = new Image("image.png", scale, scale, true, true);
 		//imageView = new ImageView(im);
 	}
