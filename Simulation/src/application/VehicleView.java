e23fb040df9ff063e194f905155b1e2f4284ad26
Carolyn <Carolyn@SamwiseGamgee.dhcp.nd.edu>
2017-11-14 12:03:41 -0500

    Grid and VehicleView JavaDocs

--- Simulation/src/application/VehicleView.java
+++ Simulation/src/application/VehicleView.java
@@ -3,6 +3,17 @@ package application;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * Enables isolated access to each vehicle object and its corresponding ImageView
+ * vehicle is the contained instance of the Vehicle class
+ * imageView is the contained instance of ImageView
+ * moveCount keeps track of the number of moves the vehicle has taken (though I'm not sure it's being used)
+ * inSimulation is a toggle boolean that is flipped off whenever vehicle exits simulation bounds
+ * 
+ * @author Carolyn
+ *
+ */
+
 public class VehicleView {
 	Vehicle vehicle;
 	ImageView imageView;
