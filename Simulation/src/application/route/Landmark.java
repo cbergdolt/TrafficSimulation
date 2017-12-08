f730cb9ac0b4cf6504df1c3726be1b31a0e8e26f
Esmeralda Cervantes <ecervant@nd.edu>
2017-12-07 20:25:54 -0500

    landmark class edits

--- Simulation/src/application/route/Landmark.java
+++ Simulation/src/application/route/Landmark.java
@@ -5,31 +5,20 @@ import java.awt.Point;
 /***
  * Landmark class to hold the main destination stops
  * for the vehicles
+ * 
+ * @author Femgineers
  *
  */
 
 public class Landmark {
-	//String name;
 	int id;
 	Point[] location; 
-	//Point disappearA; // point on road to determine when to disappear 
-	//Point disappearB;
 	LandmarkType type;
-	// will always only be one spot N or S or E or W
-	// add new direction L to not render / increment counter in vehicle
-	// create function to 
-	// multiples of 7 reserved for roundabout in grid
-	// in intersection 
-	// entry and exit in the intersection
-	
 	
 	public Landmark(int num, Point[] loc, LandmarkType t){
-		//name = tag;
 		id = num;
 		location = loc;
 		type = t;
-		//disappearA = a;
-		//disappearB = b;
 	}
 	
 	public void setDisappearA(int x, int y) {
@@ -43,6 +32,7 @@ public class Landmark {
 	public Point getLocation() {
 		return location[0];
 	}
+	
 	public Point[] getLocationArray() { return location; }
 	
 	public LandmarkType getType() { return type; }
